b6842451da484d2107f3a451087be7c0b6e58f51
arthursleite <arthurmais4@gmail.com>
2024-11-22 04:32:27 -0300

    integracao completa, todos os requisitos essenciais estao funcionando e todos os dados estao sendo puxados

--- src/routes/eventoRoutes.js
+++ src/routes/eventoRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const eventoController = require('../controllers/eventoController');
+const carregarDadosCompartilhados = require('../middlewares/dadosCompartilhados');
 
 //GET
-router.get('/criar_evento', eventoController.exibirCriarEvento);
-router.get('/detalhes_evento/:id', eventoController.exibirDetalhesEvento);
-router.get('/editar_evento/:id', eventoController.exibirEditarEvento);
+router.get('/criar_evento', carregarDadosCompartilhados, eventoController.exibirCriarEvento);
+router.get('/detalhes_evento/:id', carregarDadosCompartilhados, eventoController.exibirDetalhesEvento);
+router.get('/editar_evento/:id', carregarDadosCompartilhados, eventoController.exibirEditarEvento);
+router.get('/eventos', carregarDadosCompartilhados, eventoController.exibirPaginaEventos);
 
 //POST
 router.post('/criar_evento', eventoController.criarEvento);
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+const carregarDadosCompartilhados = require('../middlewares/dadosCompartilhados');
 
 //GET
-router.get('/home_usuario', usuarioController.listarUsuarios);
-router.get('/cadastro', usuarioController.exibirCriarUsuario);
-router.get('/perfil_usuario/:id', usuarioController.exibirDetalhesUsuario);
-router.get('/editar_usuario/:id', usuarioController.exibirEditarUsuario);
+router.get('/home_usuario', carregarDadosCompartilhados, usuarioController.listarUsuarios);
+router.get('/cadastro', carregarDadosCompartilhados, usuarioController.exibirCriarUsuario);
+router.get('/perfil_usuario/:id', carregarDadosCompartilhados, usuarioController.exibirPerfilUsuario);
+router.get('/seguranca_acesso/:id', carregarDadosCompartilhados, usuarioController.exibirSegurancaAcessoUsuario);
 
 //POST
 router.post('/cadastro', usuarioController.criarUsuario);
-router.post('/editar_usuario/:id', usuarioController.editarUsuario);
+router.post('/editar_dados_pessoais/:id', usuarioController.editarDadosPessoais);
+router.post('/editar_dados_seguranca/:id', usuarioController.editarDadosSeguranca);
 router.post('/excluir_usuario/:id', usuarioController.excluirUsuario);
 router.post('/inscrever_no_evento/:id', usuarioController.adicionarUsuarioAoEvento);
 router.post('/desinscrever_do_evento/:id', usuarioController.removerUsuarioDoEvento);
